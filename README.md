7d8dfed56bf156b2800b0cb4791e6b38ad0740b0
Milad Khanibeik <milad.khanibeik@exxeta.com>
2020-05-05 14:33:03 +0200

    add some dummy books

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # nodejs-library
 A Sample Nodejs Library Application
+
+
+### insert data to DB:
+
+Make sure mongo db is running. If not run the following command:
+
+`mongod --config /usr/local/etc/mongod.conf`
+
+Then run the following command to insert dummy books to the database:
+
+`mongo bookAPI < resources/booksJson.js`
---
+++ resources/booksJson.js
@@ -0,0 +1,27 @@
+db.books.insert([
+    {
+      title: 'War and Peace',
+      genre: 'Historical Fiction',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false
+    },
+    {
+      title: 'Les Miserables',
+      genre: 'Historical Fiction',
+      author: 'Victor Hugo',
+      read: false
+    },
+    {
+      title: 'The Time Machine',
+      genre: 'Science Fiction',
+      author: 'H. G. Wells',
+      read: false
+    },
+    {
+      title: 'A Journy into the Center of the Earh',
+      genre: 'Sicence Fiction',
+      author: 'Jules Verne',
+      read: false
+    }
+  ]);
+  
