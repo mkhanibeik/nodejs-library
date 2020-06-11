7937c85ddb88b02143af4e45eab6d3608ffd73f0
Milad Khanibeik <milad.khanibeik@exxeta.com>
2020-06-11 22:28:06 +0200

    fixing unit tests

--- tests/bookController.test.js
+++ tests/bookController.test.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-underscore-dangle */
+/* eslint-disable global-require */
 /* eslint-disable no-undef */
 const bookController = require('../controllers/bookController');
 const mockBooks = require('./mockBooks');
-
-// const books = require('./mockBooks');
 
 const mockRequest = (queryIn, bodyIn) => ({
   query: queryIn || {},
@@ -13,15 +11,24 @@ const mockRequest = (queryIn, bodyIn) => ({
 jest.mock('../models/book', () => () => {
   const SequelizeMock = require('sequelize-mock');
   const books = require('./mockBooks');
+
   const dbMock = new SequelizeMock();
   const model = dbMock.define('book');
-  model.$queueResult([
-    model.build(books[0]),
-    model.build(books[1]),
-    model.build(books[2]),
-    model.build(books[3]),
-    model.build(books[4])
-  ]);
+  model.$queueResult(
+    books.map((book) => model.build(book))
+  );
+  model.$queryInterface.$useHandler((query, options) => {
+    if (query === 'findOne') {
+      const foundBook = books.find((book) => book.id === options[0].where.id);
+      // promise should be used to really return undefined
+      if (!foundBook) return Promise.resolve(undefined);
+      return foundBook;
+    } if (query === 'findAll' && options[0].where.genre) {
+      return books.filter((book) => book.genre === options[0].where.genre);
+    }
+    // in case of undefined, the default callback is called
+    return undefined;
+  });
   return model;
 });
 
@@ -42,172 +49,167 @@ describe('Book Controller Tests:', () => {
       );
     });
 
-    // it('should all Crime books be returned.', async () => {
-    //   // given
-    //   const req = mockRequest({ genre: 'Crime' });
-    //   const crimeBooks = mockBooks.filter((book) => book.genre === 'Crime');
+    it('should all Crime books be returned.', async () => {
+      // given
+      const req = mockRequest({ genre: 'Crime' });
+      const crimeBooks = mockBooks.filter((book) => book.genre === 'Crime');
 
-    //   // when
-    //   const controller = bookController();
-    //   const returnedBooks = await controller.getAll(req);
+      // when
+      const controller = bookController();
+      const returnedBooks = await controller.getAll(req);
 
-    //   // then
-    //   expect(returnedBooks).toHaveLength(crimeBooks.length);
-    //   expect(returnedBooks).toContainEqual(
-    //     expect.objectContaining({ id: expect.anything() })
-    //   );
-    // });
+      // then
+      expect(returnedBooks).toHaveLength(crimeBooks.length);
+      expect(returnedBooks).toContainEqual(
+        expect.objectContaining({ id: expect.anything() })
+      );
+    });
   });
 
-  // describe('Get One Book', () => {
-  //   it('should return one book.', async () => {
-  //     // given
-  //     const bookId = '507f191e810c19729de860ea';
-  //     const mockBook = mockBooks[0];
-  //     mockBook._id = bookId;
-  //     mockingoose(model).toReturn(mockBook, 'findOne');
+  describe('Get One Book', () => {
+    it('should return one book.', async () => {
+      // given
+      const bookId = 1;
+      const mockBook = mockBooks.find((book) => book.id === bookId);
 
-  //     // when
-  //     const controller = bookController(model);
-  //     const returnedBook = await controller.getOne(bookId);
+      // when
+      const controller = bookController();
+      const returnedBook = await controller.getOne(bookId);
 
-  //     // then
-  //     expect(JSON.parse(JSON.stringify(returnedBook))).toMatchObject(mockBook);
-  //   });
+      // then
+      expect(returnedBook).toMatchObject(mockBook);
+    });
 
-  //   it('should return no book.', async () => {
-  //     // given
-  //     const bookId = '507f191e810c19729de860ea';
-  //     mockingoose(model).toReturn(undefined, 'findOne');
+    it('should return no book.', async () => {
+      // given
+      const bookId = 34543564;
 
-  //     // when
-  //     const controller = bookController(model);
-  //     const returnedBook = await controller.getOne(bookId);
+      // when
+      const controller = bookController();
+      const returnedBook = await controller.getOne(bookId);
 
-  //     // then
-  //     expect(returnedBook).toBeUndefined();
-  //   });
-  // });
+      // then
+      expect(returnedBook).toBeUndefined();
+    });
+  });
 
-  // describe('Create Book', () => {
-  //   it('should return a book with id.', async () => {
-  //     // given
-  //     const book = {
-  //       title: 'The Time Machine',
-  //       genre: 'Science Fiction',
-  //       author: 'H. G. Wells',
-  //       read: false
-  //     };
-  //     mockingoose(model).toReturn(book, 'save');
+  describe('Create Book', () => {
+    it('should return a book with id.', async () => {
+      // given
+      const book = {
+        title: 'The Time Machine',
+        genre: 'Science Fiction',
+        author: 'H. G. Wells',
+        read: false
+      };
 
-  //     // when
-  //     const controller = bookController(model);
-  //     const returnedBook = await controller.create(book);
+      // when
+      const controller = bookController();
+      const returnedBook = await controller.create(book);
 
-  //     // then
-  //     expect(returnedBook).toHaveProperty('_id');
-  //   });
+      // then
+      expect(returnedBook).toHaveProperty('id');
+      expect(returnedBook.title).toBe(book.title);
+    });
 
-  //   it('Should not allow empty title.', async () => {
-  //     // given
-  //     const book = {
-  //       genre: 'Science Fiction',
-  //       author: 'H. G. Wells',
-  //       read: false
-  //     };
+    it('Should not allow empty title.', async () => {
+      // given
+      const book = {
+        genre: 'Science Fiction',
+        author: 'H. G. Wells',
+        read: false
+      };
 
-  //     // when
-  //     const controller = bookController(model);
-  //     await expect(controller.create(book))
-  //       // then
-  //       .rejects.toThrow('Title is required');
-  //   });
-  // });
+      // when
+      const controller = bookController();
+      await expect(controller.create(book))
+        // then
+        .rejects.toThrow('Title is required');
+    });
+  });
 
-  // describe('Update Book', () => {
-  //   it('should return updated book.', async () => {
-  //     // given
-  //     const bookId = '507f191e810c19729de860ea';
-  //     const mockBook = mockBooks[0];
-  //     mockBook._id = bookId;
-  //     const updatedBook = {
-  //       _id: bookId,
-  //       title: 'The Time Machine',
-  //       genre: 'Science Fiction',
-  //       author: 'H. G. Wells',
-  //       read: true
-  //     };
-  //     mockingoose(model).toReturn(updatedBook, 'save');
+  describe('Update Book', () => {
+    it('should return updated book.', async () => {
+      // given
+      const bookId = 1;
+      const mockBook = mockBooks.find((book) => book.id === bookId);
+      const updatedBook = {
+        id: bookId,
+        title: 'The Time Machine',
+        genre: 'Science Fiction',
+        author: 'H. G. Wells',
+        read: true
+      };
+      mockBook.update = () => updatedBook;
 
-  //     // when
-  //     const controller = bookController(model);
-  //     const returnedBook = await controller.update(mockBook, updatedBook);
+      // when
+      const controller = bookController();
+      const returnedBook = await controller.update(mockBook, updatedBook);
 
-  //     // then
-  //     expect(JSON.parse(JSON.stringify(returnedBook))).toMatchObject(updatedBook);
-  //   });
+      // then
+      expect(returnedBook).toMatchObject(updatedBook);
+    });
 
-  //   it('Should not allow empty title.', async () => {
-  //     // given
-  //     const bookId = '507f191e810c19729de860ea';
-  //     const mockBook = mockBooks[0];
-  //     mockBook._id = bookId;
-  //     const updatedBook = {
-  //       genre: 'Science Fiction',
-  //       author: 'H. G. Wells',
-  //       read: false
-  //     };
+    it('Should not allow empty title.', async () => {
+      // given
+      const bookId = 1;
+      const mockBook = mockBooks.find((book) => book.id === bookId);
+      const updatedBook = {
+        id: bookId,
+        genre: 'Science Fiction',
+        author: 'H. G. Wells',
+        read: true
+      };
 
-  //     // when
-  //     const controller = bookController(model);
-  //     await expect(controller.update(mockBook, updatedBook))
-  //       // then
-  //       .rejects.toThrow('Title is required');
-  //   });
-  // });
+      // when
+      const controller = bookController();
+      await expect(controller.update(mockBook, updatedBook))
+        // then
+        .rejects.toThrow('Title is required');
+    });
+  });
 
-  // describe('Patch Book', () => {
-  //   it('should return patched book.', async () => {
-  //     // given
-  //     const bookId = '507f191e810c19729de860ea';
-  //     const mockBook = mockBooks[0];
-  //     mockBook._id = bookId;
-  //     const patchedBook = {
-  //       read: true
-  //     };
-  //     const updatedBook = mockBook;
-  //     updatedBook.read = true;
-  //     mockingoose(model).toReturn(updatedBook, 'save');
+  describe('Patch Book', () => {
+    it('should return patched book.', async () => {
+      // given
+      const bookId = 1;
+      const mockBook = mockBooks.find((book) => book.id === bookId);
+      const patchedBook = {
+        read: true
+      };
+      const updatedBook = mockBook;
+      updatedBook.read = true;
+      mockBook.update = () => updatedBook;
 
-  //     // when
-  //     const controller = bookController(model);
-  //     const returnedBook = await controller.patch(mockBook, patchedBook);
+      // when
+      const controller = bookController();
+      const returnedBook = await controller.patch(mockBook, patchedBook);
 
-  //     // then
-  //     expect(JSON.parse(JSON.stringify(returnedBook))).toMatchObject(updatedBook);
-  //   });
+      // then
+      expect(returnedBook).toMatchObject(updatedBook);
+    });
 
-  //   it('id should not be updated.', async () => {
-  //     // given
-  //     const bookId = '507f191e810c19729de860ea';
-  //     const mockBook = mockBooks[0];
-  //     mockBook._id = bookId;
-  //     const patchedBook = {
-  //       _id: '3243242342342355',
-  //       read: true
-  //     };
-  //     const updatedBook = mockBook;
-  //     updatedBook.read = true;
-  //     mockingoose(model).toReturn(updatedBook, 'save');
+    it('id should not be updated.', async () => {
+      // given
+      // given
+      const bookId = 1;
+      const mockBook = mockBooks.find((book) => book.id === bookId);
+      const patchedBook = {
+        id: 3434,
+        read: true
+      };
+      const updatedBook = mockBook;
+      updatedBook.read = true;
+      mockBook.update = () => updatedBook;
 
-  //     // when
-  //     const controller = bookController(model);
-  //     const returnedBook = await controller.patch(mockBook, patchedBook);
+      // when
+      const controller = bookController();
+      const returnedBook = await controller.patch(mockBook, patchedBook);
 
-  //     // then
-  //     expect(JSON.parse(JSON.stringify(returnedBook))).toMatchObject(updatedBook);
-  //   });
-  // });
+      // then
+      expect(returnedBook).toMatchObject(updatedBook);
+    });
+  });
 
   // describe('Delete Book', () => {
   //   it('should return deleted book.', async () => {
