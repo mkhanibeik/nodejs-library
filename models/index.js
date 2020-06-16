cbdad3ca324fc881c65bfccd26f10725bde54fee
Milad Khanibeik <milad.khanibeik@exxeta.com>
2020-06-16 21:05:08 +0200

    improve code syntax

--- models/index.js
+++ models/index.js
@@ -23,11 +23,7 @@ fs
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+Object.keys(db).forEach(modelName => db[modelName].associate && db[modelName].associate(db));
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
